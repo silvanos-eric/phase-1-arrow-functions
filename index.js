3abecee27cd25c5eb15fe24058b44c6b50af70c3
Silvanos Eric <ericoderito@gmail.com>
2024-07-08 06:26:39 +0300

    feat: add basic arithmetic functions to index.js
    - Implement divide function for dividing two numbers
    - Add square function using arrow syntax for squaring a number
    - Include add function using arrow syntax for addition of two numbers

---
+++ index.js
@@ -0,0 +1,15 @@
+// divide
+function divide(a, b) {
+  return a / b;
+}
+
+const a = 2000;
+const b = 100;
+
+divide(a, b);
+
+// square
+const square = (param) => param * param;
+
+// add
+const add = (a, b) => a + b;
